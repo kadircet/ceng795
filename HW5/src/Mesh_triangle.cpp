56213647e5329e2cd5af69319f34531cb5df19cf
Bilal Yaylak <bilalyaylak@gmail.com>
2018-05-06 07:25:43 +0300

    Some formatting

--- HW5/src/Mesh_triangle.cpp
+++ HW5/src/Mesh_triangle.cpp
@@ -85,22 +85,22 @@ bool Mesh_triangle::intersect(const Ray& ray, Hit_data& hit_data) const {
     switch (triangle_shading_mode) {
       case tsm_smooth:
         normal = ((1 - beta - gamma) * vertex_0.get_vertex_normal() +
-                           beta * vertex_1.get_vertex_normal() +
-                           gamma * vertex_2.get_vertex_normal())
-        .normalize();
+                  beta * vertex_1.get_vertex_normal() +
+                  gamma * vertex_2.get_vertex_normal())
+                     .normalize();
         break;
       case tsm_flat:
         normal = this->normal;
         break;
     }
     if (texture_id != -1) {
-      //TODO: Maybe do this calculations outside to give different textures to different mesh instances like materials
+      // TODO: Maybe do this calculations outside to give different textures to
+      // different mesh instances like materials
       const Texture& texture = scene_->textures[texture_id];
-      if(texture.is_perlin_noise())
-      {
-        perlin_value = texture.get_perlin_noise()->get_value_at(local_intersection_point);
-        if(texture.is_bump())
-        {
+      if (texture.is_perlin_noise()) {
+        perlin_value =
+            texture.get_perlin_noise()->get_value_at(local_intersection_point);
+        if (texture.is_bump()) {
           normal = texture.bump_normal(normal, local_intersection_point);
         }
       } else {
@@ -109,25 +109,25 @@ bool Mesh_triangle::intersect(const Ray& ray, Hit_data& hit_data) const {
         Vector3 uvc = scene_->texture_coord_data[index_2 + texture_offset];
         u = uva.x + beta * (uvb.x - uva.x) + gamma * (uvc.x - uva.x);
         v = uva.y + beta * (uvb.y - uva.y) + gamma * (uvc.y - uva.y);
-        if(texture.is_bump())
-        {
-          //calculate gradients
-          float ub_ua = uvb.x-uva.x;
-          float uc_ua = uvc.x-uva.x;
-          float vb_va = uvb.y-uva.y;
-          float vc_va = uvc.y-uva.y;
-          Vector3 pb_pa = p_1-p_0;
-          Vector3 pc_pa = p_2-p_0;
-          float inverse_constant = (ub_ua*vc_va)-(vb_va*uc_ua);
-          if(inverse_constant == 0.0f) {
-            std::cerr<<"Inverse constant == 0 at triangle bump mapping" << std::endl;
+        if (texture.is_bump()) {
+          // calculate gradients
+          float ub_ua = uvb.x - uva.x;
+          float uc_ua = uvc.x - uva.x;
+          float vb_va = uvb.y - uva.y;
+          float vc_va = uvc.y - uva.y;
+          Vector3 pb_pa = p_1 - p_0;
+          Vector3 pc_pa = p_2 - p_0;
+          float inverse_constant = (ub_ua * vc_va) - (vb_va * uc_ua);
+          if (inverse_constant == 0.0f) {
+            std::cerr << "Inverse constant == 0 at triangle bump mapping"
+                      << std::endl;
             inverse_constant = 0.000001f;
           }
-          inverse_constant = 1.0f/inverse_constant;
-          Vector3 dp_du = inverse_constant * (vc_va*pb_pa-vb_va*pc_pa);
-          Vector3 dp_dv = inverse_constant * (ub_ua*pc_pa-uc_ua*pb_pa);
-          //std::cerr<<normal<<std::endl<<std::endl;
-          //std::cerr<<dp_dv.cross(dp_du).normalize()<<std::endl;
+          inverse_constant = 1.0f / inverse_constant;
+          Vector3 dp_du = inverse_constant * (vc_va * pb_pa - vb_va * pc_pa);
+          Vector3 dp_dv = inverse_constant * (ub_ua * pc_pa - uc_ua * pb_pa);
+          // std::cerr<<normal<<std::endl<<std::endl;
+          // std::cerr<<dp_dv.cross(dp_du).normalize()<<std::endl;
           normal = texture.bump_normal(dp_du, dp_dv, normal, u, v);
         }
       }
--- HW5/src/Sphere.cpp
+++ HW5/src/Sphere.cpp
@@ -1,24 +1,25 @@
 #include "Sphere.h"
-#include "Texture.h"
-#include "Scene.h"
 #include "Ray.h"
+#include "Scene.h"
+#include "Texture.h"
 
-Sphere::Sphere(const Scene* scene, const Vector3& center, float radius, int material_id, int texture_id,
-       const Transformation& transformation, const Vector3& velocity)
-: center(center),
-radius(radius),
-material_id(material_id),
-texture_id(texture_id),
-velocity(velocity),
-transformation_(transformation),
-scene_(scene) {
+Sphere::Sphere(const Scene* scene, const Vector3& center, float radius,
+               int material_id, int texture_id,
+               const Transformation& transformation, const Vector3& velocity)
+    : center(center),
+      radius(radius),
+      material_id(material_id),
+      texture_id(texture_id),
+      velocity(velocity),
+      transformation_(transformation),
+      scene_(scene) {
   is_identity_ = transformation_.get_transformation_matrix().is_identity();
   const Vector3 delta(radius);
   if (is_identity_) {
     bounding_box_ = Bounding_box(center - delta, center + delta);
   } else {
     bounding_box_ = Bounding_box::apply_transform(
-                                                  Bounding_box(center - delta, center + delta), transformation_);
+        Bounding_box(center - delta, center + delta), transformation_);
   }
   if (velocity != Vector3(0.0f)) {
     Vector3 min = bounding_box_.min_corner;
@@ -31,14 +32,13 @@ scene_(scene) {
 bool Sphere::intersect(const Ray& ray, Hit_data& hit_data) const {
   static const Vector3 zero_vector(0.0f);
   Vector3 delta_position = ray.time * velocity;
-  Translation translation(delta_position.x, delta_position.y,
-                          delta_position.z);
+  Translation translation(delta_position.x, delta_position.y, delta_position.z);
   Matrix4x4 translated_transformation_matrix =
-  translation.get_transformation_matrix() *
-  transformation_.get_transformation_matrix();
+      translation.get_transformation_matrix() *
+      transformation_.get_transformation_matrix();
   Arbitrary_transformation transformation(translated_transformation_matrix);
   const Matrix4x4& inverse_transformation =
-  transformation.get_inverse_transformation_matrix();
+      transformation.get_inverse_transformation_matrix();
   const Ray ray_local(inverse_transformation.multiply(ray.o),
                       inverse_transformation.multiply(ray.d, true),
                       ray.ray_type);
@@ -62,29 +62,32 @@ bool Sphere::intersect(const Ray& ray, Hit_data& hit_data) const {
     }
   }
   const Matrix4x4& normal_transformation =
-  transformation.get_normal_transformation_matrix();
+      transformation.get_normal_transformation_matrix();
   Vector3 local_intersection_point = ray_local.point_at(hit_data.t);
-  Vector3 local_coordinates = local_intersection_point-center;
+  Vector3 local_coordinates = local_intersection_point - center;
   Vector3 normal = local_coordinates.normalize();
   float u = -1;
   float v = -1;
   float perlin_value = -1;
-  if (texture_id != -1 ) {
-    get_uv(local_coordinates,u,v);
+  if (texture_id != -1) {
+    get_uv(local_coordinates, u, v);
     const Texture& texture = scene_->textures[texture_id];
-    if(texture.is_perlin_noise())
-    {
-      perlin_value = texture.get_perlin_noise()->get_value_at(local_intersection_point);
-      if(texture.is_bump()) {
+    if (texture.is_perlin_noise()) {
+      perlin_value =
+          texture.get_perlin_noise()->get_value_at(local_intersection_point);
+      if (texture.is_bump()) {
         normal = texture.bump_normal(normal, local_intersection_point);
       }
     } else {
-      if(texture.is_bump()) {
-        //calculate gradient vectors
+      if (texture.is_bump()) {
+        // calculate gradient vectors
         float theta = M_PI * v;
-        float phi   = M_PI * (1 - 2*u);
-        Vector3 dp_du(local_coordinates.z * 2 * M_PI , 0, local_coordinates.x * -2 * M_PI);
-        Vector3 dp_dv(local_coordinates.y * M_PI * cos(phi), -radius * M_PI * sin(theta) , local_coordinates.y * M_PI * sin(phi));
+        float phi = M_PI * (1 - 2 * u);
+        Vector3 dp_du(local_coordinates.z * 2 * M_PI, 0,
+                      local_coordinates.x * -2 * M_PI);
+        Vector3 dp_dv(local_coordinates.y * M_PI * cos(phi),
+                      -radius * M_PI * sin(theta),
+                      local_coordinates.y * M_PI * sin(phi));
         normal = texture.bump_normal(dp_du, dp_dv, normal, u, v);
       }
     }
@@ -92,15 +95,16 @@ bool Sphere::intersect(const Ray& ray, Hit_data& hit_data) const {
   hit_data.u = u;
   hit_data.v = v;
   hit_data.perlin_value = perlin_value;
-  hit_data.normal = normal_transformation.multiply(normal,true).normalize();;
+  hit_data.normal = normal_transformation.multiply(normal, true).normalize();
+  ;
   hit_data.shape = this;
   return true;
 }
 
-void Sphere::get_uv(const Vector3 &local_coordinates, float &u, float &v) const {
-  float theta = acos(local_coordinates.y/radius);
-  float phi = atan2(local_coordinates.z,local_coordinates.x);
-  u = (M_PI - phi) / (2*M_PI);
+void Sphere::get_uv(const Vector3& local_coordinates, float& u,
+                    float& v) const {
+  float theta = acos(local_coordinates.y / radius);
+  float phi = atan2(local_coordinates.z, local_coordinates.x);
+  u = (M_PI - phi) / (2 * M_PI);
   v = theta / M_PI;
 }
-
