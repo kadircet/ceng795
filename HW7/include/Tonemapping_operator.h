361081f9f1e0e2b41784708a3f9da9109821a058
Kadir Cetinkaya <kadircet@google.com>
2020-08-16 16:44:33 +0200

    add virtual destructor for abstract class

--- HW7/include/Tonemapping_operator.h
+++ HW7/include/Tonemapping_operator.h
@@ -2,10 +2,12 @@
 #ifndef TONEMAPPING_OPERATOR_H_
 #define TONEMAPPING_OPERATOR_H_
 #include <vector>
+
 #include "Vector3.h"
 class Tonemapping_operator {
  public:
   virtual void apply_tmo(const std::vector<Vector3>& input,
                          std::vector<Vector3>& output) const = 0;
+  virtual ~Tonemapping_operator() = default;
 };
 #endif
